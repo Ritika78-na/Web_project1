f4a3cf01fcb0d95b10ecca9a0107880f4b2fd008
Ritika78-na <raniritika751@gmail.com>
2026-04-29 19:10:59 +0530

    Update script1.js

--- js1/script1.js
+++ js1/script1.js
@@ -1,7 +1,7 @@
 var btn= document.querySelector('button')
 var box = document.querySelector('#box')
 btn.addEventListener("click",function(){
-    var c1=Math.floor(Math.random()*256)   // color range highest is 255 
+    var c1=Math.floor(Math.random()*256)  // color range from 0 to 255;
     var c2=Math.floor(Math.random()*256)
     var c3=Math.floor(Math.random()*256)
     console.log(c1,c2,c3)
